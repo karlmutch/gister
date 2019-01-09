cc1f320bfc7a3b567c2f0acd08938deee6373bda
Karl Mutch <karlmutch@gmail.com>
2019-01-08 16:31:45 -0800

    cowardly using dep and also ported logging from other packages to ease debugging

--- gister.go
+++ gister.go
@@ -8,35 +8,28 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
 	"strings"
 
-	"github.com/satori/go.uuid"
+	"github.com/gofrs/uuid"
+	"github.com/leaf-ai/studio-go-runner/pkg/studio"
+
+	"github.com/karlmutch/errors"
+	"github.com/karlmutch/stack"
 )
 
-// Version defines the app version
-const VERSION = "v0.4.0"
-
-// Defines different constants used
-// GIT_IO_URL is the Github's URL shortner
-// API v3 is the current version of GitHub API
 const (
-	GITHUB_API_URL = "https://api.github.com/"
-	BASE_PATH      = "/api/v3"
-	GIT_IO_URL     = "https://git.io"
+	// Version defines the app version
+	VERSION = "v0.4.0"
+
+	USER_AGENT = "gister/" + VERSION
 )
 
-//User agent defines a custom agent (required by GitHub)
-//`token` stores the GISTER_GITHUB_TOKEN from the env variables
-// GISTER_GITHUB_TOKEN must be in format of `username:token`
 var (
-	USER_AGENT = "gist/" + VERSION
-	token      = os.Getenv("GISTER_GITHUB_TOKEN")
-)
+	logger = studio.NewLogger("gister")
 
-// Variables used in `Gist` struct
-var (
 	public      bool
 	description string
 	anonymous   bool
@@ -51,21 +44,62 @@ type GistFile struct {
 
 // The required structure for POST data for API purposes
 type Gist struct {
-	Description string              `json:"description",omitempty`
+	Description string              `json:"description,omitempty"`
 	Public      bool                `json:"public"`
 	GistFile    map[string]GistFile `json:"files"`
 }
 
-//This function loads the GISTER_GITHUB_TOKEN from a '$HOME/.gist' file
-//from the user's home directory.
-func loadTokenFromFile() (token string) {
-	//get the tokenfile
-	file := filepath.Join(os.Getenv("HOME"), ".gist")
-	github_token, err := ioutil.ReadFile(file)
-	if err != nil {
-		log.Fatal(err)
+// loadTokenFromFile loads the GISTER_GITHUB_TOKEN from a '$HOME/.gist' file
+// from the user's home directory.
+func loadToken() (token string, err errors.Error) {
+	// GISTER_GITHUB_TOKEN must be in format of `username:token`
+	if token = os.Getenv("GISTER_GITHUB_TOKEN"); len(token) != 0 {
+		return token, nil
 	}
-	return strings.TrimSpace(string(github_token))
+
+	// Fall back to attempting to read from the config file
+	file := filepath.Join(os.Getenv("HOME"), ".gist")
+	github_token, errGo := ioutil.ReadFile(file)
+	if errGo != nil {
+		return "", errors.Wrap(errGo).With("file", file).With("stack", stack.Trace().TrimRuntime())
+	}
+	return strings.TrimSpace(string(github_token)), nil
+}
+
+func getGist(names []string) (gist *Gist, err errors.Error) {
+
+	// create a gist from the files array
+	gist = &Gist{
+		Description: strings.Join(flag.Args(), ", "),
+		Public:      false,
+		GistFile:    map[string]GistFile{},
+	}
+
+	for _, filename := range names {
+
+		if filename == "-" {
+			logger.Debug("Reading standard input")
+			content, errGo := ioutil.ReadAll(os.Stdin)
+			if errGo != nil {
+				return nil, errors.Wrap(errGo).With("file", "-").With("stack", stack.Trace().TrimRuntime())
+			}
+			uu, errGo := uuid.NewV4()
+			if errGo != nil {
+				return nil, errors.Wrap(errGo).With("file", "-").With("stack", stack.Trace().TrimRuntime())
+			}
+			gist.GistFile[uu.String()] = GistFile{string(content)}
+			continue
+		}
+
+		logger.Debug("Reading file: " + filename)
+		content, errGo := ioutil.ReadFile(filename)
+		if errGo != nil {
+			return nil, errors.Wrap(errGo).With("file", filename).With("stack", stack.Trace().TrimRuntime())
+		}
+		gist.GistFile[filepath.Base(filename)] = GistFile{string(content)}
+	}
+
+	return gist, nil
 }
 
 // Defines basic usage when program is run with the help flag
@@ -90,104 +124,83 @@ func main() {
 	flag.Usage = usage
 	flag.Parse()
 
-	files_list := flag.Args()
-	if len(files_list) == 0 {
-		log.Fatal("Error: No files specified or standard input.")
+	fileNames := flag.Args()
+	if len(fileNames) == 0 {
+		log.Fatal("Error: No input file(s), or standard input specified.")
 	}
 
-	files := map[string]GistFile{}
-
-	for _, filename := range files_list {
-		var (
-			err     error
-			name    string
-			content []byte
-		)
-
-		if filename == "-" {
-			content, err = ioutil.ReadAll(os.Stdin)
-			if err != nil {
-				log.Fatal("Stdin Error: ", err)
-			}
-			name = uuid.NewV4().String()
-		} else {
-			fmt.Println("Checking file:", filename)
-			content, err = ioutil.ReadFile(filename)
-			if err != nil {
-				log.Fatal("File Error: ", err)
-			}
-			name = filepath.Base(filename)
-		}
-
-		// gists api doesn't allow / on filenames
-		files[name] = GistFile{string(content)}
-	}
-
-	if description == "" {
-		description = strings.Join(files_list, ", ")
-	}
-
-	//create a gist from the files array
-	gist := Gist{
-		Description: description,
-		Public:      public,
-		GistFile:    files,
-	}
-
-	pfile, err := json.Marshal(gist)
+	gist, err := getGist(fileNames)
 	if err != nil {
-		log.Fatal("Cannot marshal json: ", err)
+		logger.Fatal(err.Error())
 	}
 
-	b := bytes.NewBuffer(pfile)
-	fmt.Println("Uploading...")
+	// Override defaults with the command line specified values, if they are not empty in the
+	// case of the description
+	if len(description) != 0 {
+		gist.Description = description
+	}
+	gist.Public = public
+
+	pfile, errGo := json.Marshal(*gist)
+	if errGo != nil {
+		logger.Fatal(errors.Wrap(errGo).With("stack", stack.Trace().TrimRuntime()).Error())
+	}
 
 	// Send request to API
-	post_to := GITHUB_API_URL + "gists"
-	if update != "" {
-		post_to += "/" + update
+	base, errGo := url.Parse("https://api.github.com/")
+	if errGo != nil {
+		logger.Fatal(errors.Wrap(errGo).With("stack", stack.Trace().TrimRuntime()).Error())
 	}
-	req, err := http.NewRequest("POST", post_to, b)
-	if err != nil {
-		log.Fatal("Cannot create request: ", err)
+
+	postTo := "gists"
+	if update != "" {
+		postTo += "/" + update
+	}
+	urlPath, errGo := url.Parse(postTo)
+	if errGo != nil {
+		logger.Fatal(errors.Wrap(errGo).With("stack", stack.Trace().TrimRuntime()).Error())
+	}
+
+	req, errGo := http.NewRequest("POST", base.ResolveReference(urlPath).String(), bytes.NewBuffer(pfile))
+	if errGo != nil {
+		logger.Fatal(errors.Wrap(errGo).With("stack", stack.Trace().TrimRuntime()).Error())
 	}
 
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Accept", "application/json")
+	req.Header.Set("User-Agent", USER_AGENT)
 	if !anonymous {
-		if token == "" {
-			token = loadTokenFromFile()
+		token, err := loadToken()
+		if err != nil {
+			logger.Fatal(err.Error())
 		}
 		words := strings.Split(token, ":")
 		if len(words) != 2 {
-			log.Fatalf("token must be in form `username:token`, was actually %s", token)
+			log.Fatalf("token must be in form 'username:token'")
 		}
 		req.SetBasicAuth(words[0], words[1])
 	}
 
+	logger.Debug("Uploading...")
 	client := http.Client{}
-	response, err := client.Do(req)
-	if err != nil {
-		log.Fatal("HTTP error: ", err)
+	response, errGo := client.Do(req)
+	if errGo != nil {
+		logger.Fatal(errors.Wrap(errGo).With("stack", stack.Trace().TrimRuntime()).Error())
 	}
 	defer response.Body.Close()
-	err = json.NewDecoder(response.Body).Decode(&responseObj)
-	if err != nil {
-		log.Fatal("Response JSON error: ", err)
+
+	if errGo = json.NewDecoder(response.Body).Decode(&responseObj); errGo != nil {
+		logger.Fatal(errors.Wrap(errGo).With("stack", stack.Trace().TrimRuntime()).Error())
 	}
 
 	if _, ok := responseObj["html_url"]; !ok {
-		// something went wrong
-		fmt.Println(responseObj["message"])
 		if a, ok := responseObj["errors"]; ok {
 			for i, m := range a.([]interface{}) {
 				for k, v := range m.(map[string]interface{}) {
-					fmt.Printf("%d %s: %s\n", i, k, v)
+					logger.Error(fmt.Sprintf("%d %s: %s\n", i, k, v))
 				}
 			}
 		}
-		os.Exit(1)
+		logger.Error(responseObj["message"].(string), "url", base.ResolveReference(urlPath).String())
 	}
-
-	fmt.Println(responseObj["html_url"])
 }
